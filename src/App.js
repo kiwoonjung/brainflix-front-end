c941046ece42fa66b9f781d790a36c5419c377a4
kiwoonjung <kiwoon0627@gmail.com>
2022-11-03 03:00:15 -0700

    update components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
+import VideoComponent from "./components/VideoComponent/VideoComponent";
 
 function App() {
   return (
     <div>
       <HeaderComponent />
+      <VideoComponent />
     </div>
   );
 }
