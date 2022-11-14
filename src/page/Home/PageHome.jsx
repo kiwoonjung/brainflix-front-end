a290b0e5505bb513edc48760e36ca919a11d1579
kiwoonjung <kiwoon0627@gmail.com>
2022-11-13 22:32:46 -0800

    update before submit

--- src/page/Home/PageHome.jsx
+++ src/page/Home/PageHome.jsx
@@ -99,6 +99,7 @@ export default function Home() {
       )
       .then((response) => {
         getComment();
+        console.log(response.data);
       })
       .catch((error) => console.log(error));
   }
@@ -108,6 +109,8 @@ export default function Home() {
 
     return foundDate;
   };
+
+  // commentArr = {currentVideo.comments.sort((a, b) => (b.timestamp - a.timestamp))};
 
   return (
     <div>
